40de9a6c2e4e86b3a54dc62dbcabaf3d40fb4e20
pedro <pdrolucas.dev@gmail.com>
2025-10-29 23:38:26 -0300

    feat: integrate Fastify plugins and add webhook listing route
    - Added dependencies for @fastify/cors, @fastify/swagger, @scalar/fastify-api-reference, fastify-type-provider-zod, and zod.
    - Configured Fastify server with CORS and Swagger documentation.
    - Implemented a new route to list webhooks with validation using Zod.

--- api/src/server.ts
+++ api/src/server.ts
@@ -1,11 +1,48 @@
 import { fastify } from 'fastify'
+import {
+  serializerCompiler,
+  validatorCompiler,
+  jsonSchemaTransform,
+  type ZodTypeProvider,
+} from 'fastify-type-provider-zod'
+import { fastifySwagger } from '@fastify/swagger'
+import { fastifyCors } from '@fastify/cors'
+import ScalarApiReference from '@scalar/fastify-api-reference'
+import { listWebhooks } from './routes/list-webhooks'
 
-const app = fastify()
+const app = fastify().withTypeProvider<ZodTypeProvider>()
+
+app.setValidatorCompiler(validatorCompiler)
+app.setSerializerCompiler(serializerCompiler)
+
+app.register(fastifyCors, {
+  origin: true,
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  // credentials: true,
+})
+
+app.register(fastifySwagger, {
+  openapi: {
+    info: {
+      title: 'Webhook Inspector API',
+      description: 'API for capturing and inspecting webhook requests',
+      version: '1.0.0',
+    },
+  },
+  transform: jsonSchemaTransform,
+})
+
+app.register(ScalarApiReference, {
+  routePrefix: '/docs',
+})
+
+app.register(listWebhooks)
 
 app.listen({ port: 3333, host: '0.0.0.0' }, (err, address) => {
   if (err) {
     console.error(err)
     process.exit(1)
   }
-  console.log(`Server is running on ${address}`)
+  console.log(`🔥 Webhook Inspector API is running on ${address}`)
+  console.log(`📖 API documentation is available at ${address}/docs`)
 })
